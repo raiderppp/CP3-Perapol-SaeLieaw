d10aa3e78d324a8514c99cd1712af6aec92faf45
raiderppp <perapol@Perapols-MacBook-Pro.local>
2021-03-28 08:45:41 +0700

    Create Exercise4_Perapol_S.py
    ส่ง Exercise4 Course Python 3

---
+++ Exercise4_Perapol_S.py
@@ -0,0 +1,15 @@
+#ผลสอบเก็บคะแนนนักศึกษามหาลัย A ชั้นปีที่ 2 เทอม 1
+Foundation_English_Score = 80.3
+General_Business_Score = 70.2
+Introduction_to_computer_Systems_Score = 85.5
+Computing_Programming_Score = 90.5
+
+print('--- Your Score ---')
+print('Foundation English               : คะแนนที่ได้' , Foundation_English_Score, 'คะแนน')
+print('General Business                 : คะแนนที่ได้', General_Business_Score, 'คะแนน')
+print('Introduction to Computer Systems : คะแนนที่ได้', Introduction_to_computer_Systems_Score, 'คะแนน')
+print('Computer Programming             : คะแนนที่ได้', Computing_Programming_Score, 'คะแนน')
+print('รวมคะแนนที่ได้', (Foundation_English_Score
+                         +General_Business_Score
+                         +Introduction_to_computer_Systems_Score
+                         +Computing_Programming_Score),'/ 400 คะแนน')
