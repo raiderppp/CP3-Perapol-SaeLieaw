ca36c70caffde0cf1eb378a052d40966ce36fa70
raiderppp <perapol@Perapols-MacBook-Pro.local>
2021-04-03 21:38:32 +0700

    Create Lecture50_Perapol_S.py

---
+++ Lecture50_Perapol_S.py
@@ -0,0 +1,15 @@
+def plusNumber(x,y):
+    print(x+y)
+plusNumber(20,50)
+
+def minusNumber(x,y):
+    print(x-y)
+minusNumber(100,20)
+
+def mutiplyNumber(x,y):
+    print(x*y)
+mutiplyNumber(10,3)
+
+def divideNumber(x,y):
+    print(x/y)
+divideNumber(100,5)
